f0a1e5fcfb0d2476c4041f6d0ba022564135cedb
Ben Diedrich <bldiedrich@gmail.com>
2014-09-03 20:30:04 -0700

    Trying NSGA2 optimization

--- examples.lisp
+++ examples.lisp
@@ -32,7 +32,17 @@
 		 :ref :eclipj2000
 		 :abcorr :none
 		 :mu (with-ephemeris "gm_de431.tpc"
-		       (body-vrd :sun :gm 1))))
+		       (body-vrd :earth :gm 1))))
+
+(defparameter *mars*
+  (make-instance 'body
+		 :ephemeris "de430.bsp"
+		 :name "MARS BARYCENTER"
+		 :center :ssb
+		 :ref :eclipj2000
+		 :abcorr :none
+		 :mu (with-ephemeris "gm_de431.tpc"
+		       (body-vrd :mars :gm 1))))
 
 ;;; Cartesian
 
@@ -252,9 +262,101 @@
 (let ((p *eg-ks-table*))
   (lethash (t0 s0) p
     (setf (gethash :x0-cart p)
-	  (with-slots (r v) (position-velocity *earth* t0)
-	    (make-instance 'cart-state :r r :v v)))
-    (setf (gethash :x0-spin p) 
+	  (position-velocity *earth* t0))
+    (setf (gethash :x0-spin p)
 	  (to-spinor t0 (gethash :x0-cart p) p))
     (setf (gethash :x0 p)
 	  (to-initial-ks s0 (gethash :x0-spin p) p))))
+
+;;; KS NSGA2 Earth to Mars transfer
+
+(defun euler-rotation (angles sequence ref)
+  (loop for a in angles
+     for s in sequence
+     for ssign = (signum s)
+     for r = (rotor (* (- ssign) (dual (nth (1- (abs s)) ref))) a)
+     then (rotor (* (- ssign) (dual (nth (1- (abs s)) f))) a)
+     for f = (new-frame r ref) then (new-frame r f)
+     for rs = r then (*g r rs)
+     finally (return (values f rs))))
+
+(defun ks-earth-mars-objfun (ind)
+  (with-slots (xvar) ind
+    (let* ((nvar 21)
+	   (t0 (coerce (encode-universal-time 0 0 0 27 2 2014 0) 'double-float))
+	   (n-rs (/ (1- nvar) 2))
+	   (dtf (first xvar))
+	   (tf (+ t0 dtf))
+	   (clocks (subseq xvar 1 (1+ n-rs)))
+	   (cones (subseq xvar (1+ n-rs) (length xvar)))
+	   (rs-table
+	    (loop with dtm = (/ (- tf t0) n-rs)
+	       for i below n-rs
+	       for tm = t0 then (+ tm dtm)
+	       for tfi = (+ tm dtm)
+	       for clock in clocks
+	       for cone in cones
+	       for rs = (nth-value 1 (euler-rotation (list clock cone) '(1 3) *j2000*))
+	       collect (make-hash
+			:rs rs
+			:stopfn #'(lambda (s x p) (tm x))
+			:stopval tfi
+			:stoptest #'>)))
+	   (problem
+	    (make-hash*
+	     :cb *ssb*
+	     :mu (slot-value cb 'mu)
+	     :sun *sun*
+	     :ls (slot-value sun 'ls)
+	     :ls-du (slot-value sun 'ls-du)
+	     :mass 52d0
+	     :area (convert-unit '(1260d0 m^2) 'km^2)
+	     :iframe *j2000*
+	     :nfun #'first
+	     :rs nil
+	     :t0 t0
+	     :tf tf
+	     :rs-table rs-table
+	     :s0 0d0
+	     :sf (/ pi 14)
+	     :x0-cart nil
+	     :x0-spin nil
+	     :x0 nil
+	     :hmin (/ (- sf s0) 20000)
+	     :hmax (/ (- sf s0) 20)
+	     :eom #'eom
+	     :tol 1d-12
+	     :stopfn nil
+	     :stopval nil
+	     :stoptest nil)))
+      (setf (gethash :x0-cart problem)
+	    (position-velocity *earth* t0))
+      (setf (gethash :x0-spin problem)
+	    (to-spinor t0 (gethash :x0-cart problem) problem))
+      (setf (gethash :x0 problem)
+	    (to-initial-ks (gethash :s0 problem) (gethash :x0-spin problem) problem))
+      (let* ((traj (propagate-ks-table problem))
+	     (sf (first (car (last traj))))
+	     (xf-ks (second (car (last traj))))
+	     (xf-cart (to-cartesian sf xf-ks problem))
+	     (xf-target (position-velocity *mars* (print tf)))
+	     (xferr (norminfx (- xf-target xf-cart))))
+	(values
+	 (list dtf xferr)
+	 nil))))) ; no constraints
+
+(defparameter *ks-earth-mars-options*
+  (make-instance
+   'options
+   :popsize 100
+   :ngen 200
+   :nobj 2
+   :ncon 0
+   :nvar (+ 1 10 10)
+   :minvar (cons (convert-unit '(1 month) 'sec) (make-list 20 :initial-element (- (/ pi 2d0))))
+   :maxvar (cons (convert-unit '(5 years) 'sec) (make-list 20 :initial-element (/ pi 2d0)))
+   :pcross 0.9d0
+   :pmut 0.5d0
+   :eta-c 10d0
+   :eta-m 20d0
+   :objfun #'ks-earth-mars-objfun))
